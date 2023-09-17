0f8f9c2b723f2ff548c0fd05d8b511203093bfe9
Rolando Flores <rolo@tutamail.com>
2023-09-17 01:58:28 -0600

    setup elm-watch

---
+++ src/Main.elm
@@ -0,0 +1,16 @@
+module Main exposing (main)
+
+import Html exposing (Html)
+import Html.Attributes as Attr
+
+
+main : Html msg
+main =
+    Html.div
+        [ Attr.style "margin" "3em"
+        , Attr.style "border" "1px solid"
+        , Attr.style "border-radius" "1em"
+        , Attr.style "padding" "1em"
+        ]
+        [ Html.text "This page is just static HTML, rendered by Elm."
+        ]
