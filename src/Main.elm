cc9836407834f6e7bdfe657faaae859e18b92cac
Rolando Flores <rolo@tutamail.com>
2023-09-30 21:50:52 -0600

    ver gráfica básica

--- src/Main.elm
+++ src/Main.elm
@@ -1,25 +1,55 @@
 module Main exposing (main)
 
+import Chart as C
+import Chart.Attributes as CA
+import Chart.Events as CE
+import Chart.Item as CI
 import Css.Global
-import Html.Styled as Html exposing (Html, div, text)
-import Html.Styled.Attributes as Attr exposing (css)
-import Tailwind.Breakpoints as TWBp
+import Html exposing (Html)
+import Html.Attributes as Attr
+import Html.Styled as HtmlS
+import Html.Styled.Attributes exposing (css)
+import Svg as S
+import Svg.Attributes as SA
+import Svg.Events as SE
+import Tailwind.Breakpoints as TwBp
 import Tailwind.Theme as Theme
-import Tailwind.Utilities as Tw exposing (globalStyles)
+import Tailwind.Utilities as Tw
 
 
+main : Html msg
 main =
-    div []
-        [ Css.Global.global globalStyles
-        , div
+    HtmlS.div [ css [ Tw.container, Tw.mx_auto ] ]
+        [ Css.Global.global Tw.globalStyles
+        , HtmlS.div
             [ css
                 [ Tw.m_3
                 , Tw.border_2
                 , Tw.border_r_2
-                , Tw.border_color Theme.red_400
+                , Tw.border_color Theme.red_600
                 , Tw.p_1
                 ]
             ]
-            [ text "This page is just static HTML, rendered by Elm." ]
+            [ HtmlS.text "This page is just static HTML, rendered by Elm." ]
+        , HtmlS.div
+            [ css [ Tw.max_w_screen_sm ] ]
+            [ grafica |> HtmlS.fromUnstyled ]
         ]
-        |> Html.toUnstyled
+        |> HtmlS.toUnstyled
+
+
+grafica : Html msg
+grafica =
+    C.chart
+        [ CA.width 30
+        , CA.height 40
+        ]
+        [ C.bars []
+            [ C.bar .income []
+            , C.bar .spending []
+            ]
+            [ { income = 10, spending = 2 }
+            , { income = 12, spending = 6 }
+            , { income = 18, spending = 16 }
+            ]
+        ]
